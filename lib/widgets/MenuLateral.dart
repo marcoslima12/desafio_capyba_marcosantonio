fde86f8e57bde5ff2603475f335308e1fc9d21a3
Marcos Antonio <marcosantonio-10@live.com>
2023-04-29 14:55:50 -0300

    fix/loggedArea: Insert log out button to drawe

--- lib/widgets/MenuLateral.dart
+++ lib/widgets/MenuLateral.dart
@@ -1,10 +1,33 @@
+import 'package:desafio_capyba_marcosantonio/pages/LoggedArea.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class DrawerWidget extends StatelessWidget {
+import '../pages/login.dart';
+
+class DrawerWidget extends StatefulWidget {
   const DrawerWidget({super.key});
 
   @override
+  State<DrawerWidget> createState() => _DrawerWidgetState();
+}
+
+class _DrawerWidgetState extends State<DrawerWidget> {
+  late User _user;
+
+  @override
   Widget build(BuildContext context) {
+    void _signOut() async {
+      try {
+        await FirebaseAuth.instance.signOut();
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => Login()),
+        );
+      } catch (e) {
+        print(e);
+      }
+    }
+
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
@@ -37,6 +60,13 @@ class DrawerWidget extends StatelessWidget {
             ),
           ),
           ListTile(
+            leading: Icon(Icons.home),
+            title: const Text('Home'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
             leading: Icon(Icons.person),
             title: const Text('Meu perfil'),
             onTap: () {
@@ -49,7 +79,15 @@ class DrawerWidget extends StatelessWidget {
             title: const Text('Validar email'),
             onTap: () {
               Navigator.pop(context);
-              Navigator.pushNamed(context, '/validate-email');
+            },
+          ),
+          Spacer(),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: const Text('Sair'),
+            onTap: () {
+              Navigator.pop(context);
+              _signOut();
             },
           ),
         ],
