8bb36d3d7dbf4213aa24cefc15686f4b63d7aad8
Marcos Antonio <marcosantonio-10@live.com>
2023-04-30 15:25:40 -0300

    fix/login: fixed front

--- lib/pages/signUp.dart
+++ lib/pages/signUp.dart
@@ -20,11 +20,16 @@ class SignUp extends StatefulWidget {
   State<SignUp> createState() => _SignUpState();
 }
 
-final TextEditingController _createEmailController = TextEditingController();
-final TextEditingController _createPasswordController = TextEditingController();
-bool isLoading = false;
-
 class _SignUpState extends State<SignUp> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _createEmailController = TextEditingController();
+  final TextEditingController _createPasswordController =
+      TextEditingController();
+  final TextEditingController _createPhoneController = TextEditingController();
+  final TextEditingController _createNameController = TextEditingController();
+
+  bool isLoading = false;
+
   void _signUp() async {
     try {
       setState(() => isLoading = true);
@@ -83,78 +88,83 @@ class _SignUpState extends State<SignUp> {
         title: const Text('Create New Account'),
       ),
       backgroundColor: Colors.greenAccent,
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Center(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            /* if (arquivo != null) Anexo(arquivo: arquivo),
-            ElevatedButton.icon(
-              onPressed: () => Get.to(
-                () => CameraCamera(onFile: (file) => showPreview(file)),
-              ),
-              icon: Icon(Icons.camera_alt),
-              label: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Text('Tire uma foto'),
-              ),
-            ), */
-            Image.asset('assets/capybaLogo.png', width: 100, height: 100),
-            Padding(
-                padding: EdgeInsets.only(top: 10, bottom: 5),
-                child: TextField(
-                    controller: _createEmailController,
-                    autofocus: true,
-                    keyboardType: TextInputType.text,
-                    style: TextStyle(color: Colors.green, fontSize: 20),
-                    decoration: InputDecoration(
-                        icon: Icon(Icons.email_outlined),
-                        labelText: "Email",
-                        labelStyle: TextStyle(color: Colors.white),
-                        border: OutlineInputBorder(gapPadding: 5)))),
-            Padding(
-              padding: EdgeInsets.only(top: 10, bottom: 5),
-              child: TextField(
-                  controller: _createPasswordController,
-                  autofocus: true,
-                  obscureText: true,
-                  keyboardType: TextInputType.text,
-                  style: TextStyle(color: Colors.green, fontSize: 20),
-                  decoration: InputDecoration(
-                      icon: Icon(Icons.key_outlined),
-                      labelText: "Password",
-                      labelStyle: TextStyle(color: Colors.white),
-                      border: OutlineInputBorder(gapPadding: 5))),
-            ),
-            Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 10),
-                  child: ElevatedButton(
-                    onPressed:
-                        /* final String email = _createEmailController.text;
-                      final String password = _createPasswordController.text; */
-                        /*   final User UserLoggedIn = User(email, password);
-                      print(UserLoggedIn); */
-                        _signUp,
-                    child: Text('Create new account >'),
-                  ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Center(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/capybaLogo.png', width: 100, height: 100),
+              Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    TextFormField(
+                      controller: _createNameController,
+                      autofocus: true,
+                      decoration: InputDecoration(
+                          labelText: "Name",
+                          labelStyle: TextStyle(color: Colors.white),
+                          border: OutlineInputBorder(gapPadding: 5)),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextFormField(
+                      controller: _createEmailController,
+                      autofocus: true,
+                      decoration: InputDecoration(
+                          labelText: "Email",
+                          labelStyle: TextStyle(color: Colors.white),
+                          border: OutlineInputBorder(gapPadding: 5)),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextFormField(
+                      controller: _createPasswordController,
+                      autofocus: true,
+                      decoration: InputDecoration(
+                          labelText: "Password",
+                          labelStyle: TextStyle(color: Colors.white),
+                          border: OutlineInputBorder(gapPadding: 5)),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextFormField(
+                      controller: _createPhoneController,
+                      autofocus: true,
+                      decoration: InputDecoration(
+                          labelText: "Phone",
+                          labelStyle: TextStyle(color: Colors.white),
+                          border: OutlineInputBorder(gapPadding: 5)),
+                    ),
+                  ],
                 ),
-                TextButton(
-                    onPressed: () => {
-                          Navigator.pushReplacement(context,
-                              MaterialPageRoute(builder: (context) => Login()))
-                        },
-                    child: Text("Already an user? Login >")),
-                (isLoading)
-                    ? CircularProgressIndicator(color: Colors.white)
-                    : CircularProgressIndicator(color: Colors.transparent),
-              ],
-            )
-          ],
-        )),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: ElevatedButton(
+                  onPressed: _signUp,
+                  child: Text('Create new account >'),
+                ),
+              ),
+              TextButton(
+                  onPressed: () => {
+                        Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (context) => Login()))
+                      },
+                  child: Text("Already an user? Login >")),
+              /* (isLoading)
+                  ? CircularProgressIndicator(color: Colors.white)
+                  : CircularProgressIndicator(color: Colors.transparent), */
+            ],
+          )),
+        ),
       ),
     );
   }
