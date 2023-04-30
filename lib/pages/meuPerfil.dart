4bb32c5932050d97375ae76a926e8e0a68985c0f
Marcos Antonio <marcosantonio-10@live.com>
2023-04-30 12:22:04 -0300

    feature/myProfile: Fixed style of page

--- lib/pages/meuPerfil.dart
+++ lib/pages/meuPerfil.dart
@@ -13,96 +13,85 @@ class MyProfile extends StatefulWidget {
 
 class _MyProfileState extends State<MyProfile> {
   User? currentUser = FirebaseAuth.instance.currentUser;
+  bool isEditing = false;
+  final TextEditingController _newEmailController = TextEditingController();
+  final TextEditingController _newPasswordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         drawer: DrawerWidget(),
         appBar: AppBar(
-          title: Text('Meu perfil'),
+          title: Text('Meu Perfil'),
           centerTitle: true,
-          actions: [
-            ElevatedButton.icon(
-                onPressed: () {},
-                icon: Icon(Icons.edit_outlined),
-                label: Text('Editar'),
-                )
-          ],
         ),
-        body: Center(
-            child: Column(children: [
+        body: Column(children: [
           Container(
             width: double.infinity,
-            height: 200,
-            color: Colors.green,
-            child: Center(
-                child: ClipRRect(
-              borderRadius: BorderRadius.circular(100.0), // raio dos cantos
-              child: Image.asset('assets/man.jpg', width: 100, height: 100),
-            )),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 30, right: 30, bottom: 15, top: 30),
-            child: Row(
-              children: [
-                Icon(Icons.person),
-                SizedBox(width: 20),
-                Column(
+            height: 220,
+            color: Colors.greenAccent,
+            child: Padding(
+              padding: EdgeInsets.only(top: 30, left: 30, bottom: 10),
+              child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Text('Nome', style: TextStyle(fontWeight: FontWeight.w500)),
-                    Text('Marcos Antonio')
-                  ],
-                )
-              ],
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          border: Border.all(color: Colors.white, width: 4)),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: Image.asset('assets/man.jpg',
+                            width: 90, height: 90),
+                      ),
+                    ),
+                    Text('Marcos Antonio',
+                        style: TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.bold)),
+                    Text(
+                      '${currentUser!.email}',
+                      style: TextStyle(color: Colors.black45),
+                    )
+                  ]),
             ),
           ),
-          Divider(
-            thickness: 1,
-            color: Colors.grey[400],
-            indent: 20,
-            endIndent: 20,
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 30, right: 30, bottom: 15, top: 15),
-            child: Row(
-              children: [
-                Icon(Icons.email),
+          TextButton(
+            onPressed: () {},
+            child: Padding(
+              padding: EdgeInsets.only(top: 15, left: 30, bottom: 15),
+              child: Row(children: [
+                Icon(Icons.edit_outlined),
                 SizedBox(width: 20),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('E-mail',
-                        style: TextStyle(fontWeight: FontWeight.w500)),
-                    Text('${currentUser!.email}')
-                  ],
-                )
-              ],
+                Text('Edit Profile'),
+                Icon(Icons.arrow_circle_right)
+              ]),
             ),
           ),
-          Divider(
-            thickness: 1,
-            color: Colors.grey[400],
-            indent: 20,
-            endIndent: 20,
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 30, right: 30, bottom: 15, top: 15),
-            child: Row(
-              children: [
-                Icon(Icons.calendar_month),
+          TextButton(
+            onPressed: () {},
+            child: Padding(
+              padding: EdgeInsets.only(top: 15, left: 30, bottom: 15),
+              child: Row(children: [
+                Icon(Icons.password_outlined),
                 SizedBox(width: 20),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Member since',
-                        style: TextStyle(fontWeight: FontWeight.w500)),
-                    Text('${currentUser!.metadata.creationTime!.year}')
-                  ],
-                )
-              ],
+                Text('Change Password'),
+                Icon(Icons.arrow_circle_right)
+              ]),
             ),
-          )
-        ])));
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Padding(
+              padding: EdgeInsets.only(top: 15, left: 30, bottom: 15),
+              child: Row(children: [
+                Icon(Icons.logout_outlined),
+                SizedBox(width: 20),
+                Text('Sair'),
+                Icon(Icons.arrow_circle_right)
+              ]),
+            ),
+          ),
+        ]));
   }
 }
